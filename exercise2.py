3d63ec2027fdf2e7a220b5823c51e4b3a60157d1
Delannah <collid5@uw.edu>
2019-03-12 17:56:15 -0700

    updates

--- exercise2.py
+++ exercise2.py
@@ -22,7 +22,7 @@ dcc.Graph(
             # configure the data
             'data': [
                 # set x to be weekday, and y to be the counts. We use bars to represent our data.
-                {'x': weekday_in_order, 'y': counts_in_order, 'type': 'bar', 'name': 'Total'},
+                {'x': weekday_in_order, 'y': counts_in_order, 'type': 'scatterplot', 'name': 'Total'},
                 {'x': weekday_in_order, 'y': bike_s, 'type': 'scatterplot', 'name': 'Bike_s'},
                 {'x': weekday_in_order, 'y': bike_n, 'type': 'scatterplot', 'name': 'Bike_n'},
             ],
