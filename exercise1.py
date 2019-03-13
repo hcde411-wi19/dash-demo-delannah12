95b9bb6a9bb572cb856b78e31a4a9df822a0703c
Delannah <collid5@uw.edu>
2019-03-12 17:28:35 -0700

    update

--- exercise1.py
+++ exercise1.py
@@ -35,7 +35,7 @@ app.layout = html.Div(children=[
                 # set x to be weekday, and y to be the counts. We use bars to represent our data.
                 {'x': weekday_in_order, 'y': counts_in_order, 'type': 'bar:', 'name': 'Total'},
                 {'x': weekday_in_order, 'y': ped_s, 'type': 'bar:', 'name': 'Ped_s'},
-                {'x': weekday_in_order, 'y': ped_n, 'type': 'bar:', 'name': 'Ped_n'}
+                {'x': weekday_in_order, 'y': ped_n, 'type': 'bar:', 'name': 'Ped_n'},
                 {'x': weekday_in_order, 'y': bike_s, 'type': 'bar:', 'name': 'Bike_s'},
                 {'x': weekday_in_order, 'y': bike_n, 'type': 'bar:', 'name': 'Bike_n'},
 
