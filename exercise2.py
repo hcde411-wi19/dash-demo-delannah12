ffbed33ce2ea5ad726cb87466afe5e83b08a4c37
Delannah <collid5@uw.edu>
2019-03-12 17:50:27 -0700

    exercise2

--- exercise2.py
+++ exercise2.py
@@ -3,6 +3,45 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+weekday_in_order = ['Saturday', 'Sunday']
+counts_in_order = [160613, 154225, 155175, 150819, 146014, 215725, 203483]
+bike_s = [52642, 50812, 51866, 50913, 49740, 71586, 68147]
+bike_n = [52401, 49449, 50209, 49692, 47971, 69437, 67752]
+
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(children=[
+    html.H1(children='Delannah Collins-Wright'),
+    html.Div(children='''
+    '''),
+
+dcc.Graph(
+    id='example-graph',
+        figure={
+            # configure the data
+            'data': [
+                # set x to be weekday, and y to be the counts. We use bars to represent our data.
+                {'x': weekday_in_order, 'y': counts_in_order, 'type': 'bar', 'name': 'Total'},
+                {'x': weekday_in_order, 'y': bike_s, 'type': 'scatter', 'name': 'Bike_s'},
+                {'x': weekday_in_order, 'y': bike_n, 'type': 'scatter', 'name': 'Bike_n'},
+            ],
+
+            'layout': {
+                'title': 'Comparison of Bikes'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+
+
+
+
 
 # Exercise 2
 # Create another visualization of your choice of data and chart type.
